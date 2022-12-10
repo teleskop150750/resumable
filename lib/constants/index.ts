9e47c50395a255609992492a39deed644e1bb6b1
Teleskop150750 <teleskop150750@gmail.com>
2022-12-10 17:19:56 +0300

    refactor: Удалить лишний код

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,2 +1,1 @@
 export * from './chunkStatus'
-export * from './client'
--- lib/helpers/types.ts
+++ lib/helpers/types.ts
@@ -1,3 +1,2 @@
 export type Nillable<T> = T | undefined
 export type PromiseOrValue<T> = T | Promise<T>
-export type Arrayable<T> = T | Array<T>
